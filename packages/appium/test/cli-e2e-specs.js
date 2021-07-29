d3d48eab71eab75b1ca150038b1fb9961ad4cbb1
Christopher Hiller <boneskull@boneskull.com>
2021-07-29 15:47:08 -0700

    chore(appium): more config file updates
    schema:
    
    - rename subschema `title` props since they have a direct effect on the generated declarations
    - enforce uniqueness on `allow-insecure` and `deny-insecure`
    - allow `default-capabilities` to be a path to a file
    - formatting, description updates, comments
    
    `config-file` module:
    
    - change `ReadConfigFileOptions.normalizeKeys` to `.normalize`
    - add `NormalizedAppiumConfiguration` which is like `AppiumConfiguration` except all keys are camel-cased (I _think_)
    - handle subschemas with union types
    - normalization function now just normalizes the entire config file, using `appiumDest` as appropriate
    
    main module:
    
    - remove unneeded `await`
    - correctly (_more_ correctly?) apply defaults
    - actually respect `--config`
    
    tests:
    
    - create `test/fixtures/config` for all relevant fixtures
    - fix `cli-e2e-specs` to not assume fixtures are relative to cwd
    - add more tests around handling various properties which can be of multiple types

--- packages/appium/test/cli-e2e-specs.js
+++ packages/appium/test/cli-e2e-specs.js
@@ -3,16 +3,14 @@ import path from 'path';
 import { exec } from 'teen_process';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import { tempDir, fs, mkdirp, util } from 'appium-support';
+import { tempDir, fs, mkdirp, util } from '@appium/support';
 import { KNOWN_DRIVERS } from '../lib/drivers';
 import { PROJECT_ROOT as cwd } from './helpers';
 
 chai.should();
 chai.use(chaiAsPromised);
 
-// cannot use `require.resolve()` here (w/o acrobatics) due to the ESM context.
-// could also derive it from the `package.json` if we wanted
-const executable = path.join(cwd, 'packages', 'appium', 'build', 'lib', 'main.js');
+const executable = require.resolve('../build/lib/main.js');
 
 describe('CLI', function () {
   let appiumHome;
